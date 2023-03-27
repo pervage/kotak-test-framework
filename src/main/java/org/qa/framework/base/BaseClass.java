0dea433a7ede0e3e6869b294fe9a73c7c46f58a0
Pervage Alam <alam.pervage@gmail.com>
2023-03-27 12:42:15 +0530

    Added Headless Mode for Browsers

--- src/main/java/org/qa/framework/base/BaseClass.java
+++ src/main/java/org/qa/framework/base/BaseClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.manager.SeleniumManager;
@@ -22,12 +23,20 @@ public class BaseClass {
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--remote-allow-origins=*");
                 chromeOptions.addArguments("--disable-notifications");
+                if(System.getenv("headless_mode").equals("Yes")){
+                    chromeOptions.addArguments("--headless=new");
+                }
                 webdriver = new ChromeDriver(chromeOptions);
                 log.info("Chrome Driver Initialized.");
             }
             case "FIREFOX" -> {
                 SeleniumManager.getInstance();
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if(System.getenv("headless_mode").equals("Yes")){
+                    FirefoxBinary firefoxBinary = new FirefoxBinary();
+                    firefoxBinary.addCommandLineOptions("--headless");
+                    firefoxOptions.setBinary(firefoxBinary);
+                }
                 firefoxOptions.addPreference("dom.webnotifications.enabled", false);
                 webdriver = new FirefoxDriver(firefoxOptions);
                 log.info("Firefox Driver Initialized.");
@@ -35,6 +44,9 @@ public class BaseClass {
             case "EDGE" -> {
                 SeleniumManager.getInstance();
                 EdgeOptions edgeOptions = new EdgeOptions();
+                if(System.getenv("headless_mode").equals("Yes")){
+                    edgeOptions.addArguments("headless");
+                }
                 edgeOptions.addArguments("--disable-notifications");
                 webdriver = new EdgeDriver(edgeOptions);
                 log.info("Edge Driver Initialized.");
